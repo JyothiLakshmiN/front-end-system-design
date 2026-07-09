6b09b418397b1fd5cde15ff1d938f6e79d287f19
Jyothi Lakshmi N <jyothilakshmi.cbp@gmail.com>
2026-07-09 08:52:38 -0500

    Code splitting and lazy loading

---
+++ performance/code-splitting/src/pages/FAQs.jsx
@@ -0,0 +1,10 @@
+const FAQs = () => {
+    return (
+        <div>
+            <h1>FAQs Page</h1>
+            <p>This is the FAQs page.</p>
+        </div>
+    )
+}
+
+export default FAQs;
---
+++ performance/code-splitting/src/pages/index.js
@@ -0,0 +1,5 @@
+export * from "./About";    
+export * from "./Profile";   
+export * from "./Contact";   
+export * from "./FAQs";      
+export * from "./Login";
