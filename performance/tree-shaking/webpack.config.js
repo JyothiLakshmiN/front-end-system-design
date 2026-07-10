fd0adf5623cf222a86d60b4fc15e788c301a6128
Jyothi Lakshmi N <jyothilakshmi.cbp@gmail.com>
2026-07-09 20:54:02 -0500

    treeshaking

---
+++ performance/tree-shaking/webpack.config.js
@@ -0,0 +1,36 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+const path = require('path');
+
+module.exports = {
+  entry: path.join(__dirname, 'src', 'index.js'),
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+   module: {
+    rules: [
+      {
+        test: /\.?js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: [
+              "@babel/preset-env"
+            ],
+          },
+        },
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+        template: path.join(__dirname, 'src', 'index.html')
+    })
+  ],
+  optimization: {
+    usedExports: true,
+  },
+  mode: 'development',
+};
